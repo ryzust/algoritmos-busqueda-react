052b205be2c38e0187c3584aaffb5cc0fe71a6e4
ryzust <Zrubiol1700@alumno.ipn.mx>
2022-04-17 22:56:01 -0500

    deleted the calculation of coordinates

--- src/Components/Connection/connection.component.jsx
+++ src/Components/Connection/connection.component.jsx
@@ -1,32 +1,35 @@
 import React from "react";
 
 export const Connection = ({ p1, p2, gx }) => {
-  p1[0] += 8 * 2.5;
-  p1[1] += 8 * 2.5;
-  p2[0] += 8 * 2.5;
-  p2[1] += 8 * 2.5;
-
   let left = Math.floor((p1[0] + p2[0]) / 2);
   let top = Math.floor((p1[1] + p2[1]) / 2);
+
   return (
     <>
-      <div
+      <p
         style={{
-          zIndex: -2,
+          zIndex: -1,
           position: "absolute",
           top,
           left,
-          color: "#000",
         }}
+        className="connection"
       >
         {gx}
-      </div>
+      </p>
       <svg
         style={{ zIndex: -1, position: "absolute" }}
         width="1000px"
         height="1000px"
       >
-        <line x1={p1[0]} y1={p1[1]} x2={p2[0]} y2={p2[1]} stroke="black" />
+        <line
+          style={{ position: "absolute" }}
+          x1={p1[0]}
+          y1={p1[1]}
+          x2={p2[0]}
+          y2={p2[1]}
+          stroke="black"
+        />
       </svg>
     </>
   );
