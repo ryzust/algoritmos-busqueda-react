f4ebd86aed01879d0ca3eddaa21a90f4e082b124
ryzust <Zrubiol1700@alumno.ipn.mx>
2022-04-17 23:36:24 -0500

    changed to use reducers

--- src/App.js
+++ src/App.js
@@ -9,154 +9,35 @@ import { HTML5Backend } from "react-dnd-html5-backend";
 class App extends Component {
   constructor() {
     super();
-    this.addVertice.bind(this);
-    this.deleteVertice.bind(this);
-    this.modifyVertice.bind(this);
-    this.addEdge.bind(this);
     this.state = {
-      algoritmos: ["A*", "Primero el mejor", "Ascenso a la colina","Busqueda en profundidad","Busqueda en anchura"],
-      grafo: {
-        vertices: ["wea", "wea2", "xd"],
-        h: [1, 2, 3],
-        edges: [
-          ["wea", "wea2"],
-          ["wea", "xd"],
-        ],
-        g: [5, 6],
-        start: "wea",
-        end: "xd",
-      },
+      algoritmos: [
+        "A*",
+        "Primero el mejor",
+        "Ascenso a la colina",
+        "Busqueda en profundidad",
+        "Busqueda en anchura",
+      ],
       algSelected: "A*",
     };
   }
-  addVertice = (name, hx) => {
-    this.setState((prevState) => {
-      let alreadyExists = prevState.grafo.vertices.indexOf(name);
-      if (alreadyExists === -1) {
-        return {
-          ...prevState,
-          grafo: {
-            ...prevState.grafo,
-            vertices: [...prevState.grafo.vertices, name],
-            h: [...prevState.grafo.h, parseInt(hx)],
-          },
-        };
-      } else {
-        return { ...prevState };
-      }
-    });
-    console.log("added ", name, hx);
-  };
-
-  modifyVertice = (pname, name, hx) => {
-    this.setState((prevState) => {
-      let idx = prevState.grafo.vertices.indexOf(pname);
-      let copy = { ...prevState };
-      console.log("Idx: ", idx);
-      if (idx !== -1) {
-        copy.grafo.vertices[idx] = name;
-        copy.grafo.h[idx] = hx;
-        for (let i = 0; i < copy.grafo.edges.length; i++) {
-          let s = copy.grafo.edges[i].indexOf(pname);
-
-          if (s !== -1) {
-            copy.grafo.edges[i][s] = name;
-          }
-        }
-      }
-      return { copy };
-    });
-  };
-
-  deleteVertice = (name) => {
-    this.setState((prevState) => {
-      let idx = prevState.grafo.vertices.indexOf(name);
-      let copy = { ...prevState };
-      console.log("Idx: ", idx);
-      if (idx !== -1) {
-        copy.grafo.vertices.splice(idx, 1);
-        copy.grafo.h.splice(idx, 1);
-        for (let i = 0; i < copy.grafo.edges.length; i++) {
-          let s = copy.grafo.edges[i].indexOf(name);
-
-          if (s !== -1) {
-            copy.grafo.edges.splice(idx, 1);
-          }
-        }
-      }
-      return { copy };
-    });
-  };
-
-  addEdge = (verticePair, gx) => {
-    this.setState((prevState) => {
-      let alreadyExists =
-        prevState.grafo.edges.indexOf(verticePair) !== -1 ||
-        prevState.grafo.edges.indexOf([verticePair[1], verticePair[0]]) !== -1;
-      if (!alreadyExists) {
-        return {
-          ...prevState,
-          grafo: {
-            ...prevState.grafo,
-            edges: [...prevState.grafo.edges, verticePair],
-            g: [...prevState.grafo.g, parseInt(gx)],
-          },
-        };
-      } else {
-        return { ...prevState };
-      }
-    });
-  };
 
   render() {
     return (
       <DndProvider backend={HTML5Backend}>
-        {console.log(this.state.grafo)}
         <div className="App" style={{ overflow: "hidden" }}>
           <ActionsBar
             algoritmos={this.state.algoritmos}
             sAlg={this.state.algSelected}
-            grafo={this.state.grafo}
             handleChange={(e) => {
               this.setState({ algSelected: e.target.value });
-            }}
-            handleChangeStart={(e) => {
-              this.setState((prevState) => {
-                return {
-                  ...prevState,
-                  grafo: {
-                    ...prevState.grafo,
-                    start: e.target.value,
-                  },
-                };
-              });
-            }}
-            handleChangeEnd={(e) => {
-              this.setState((prevState) => {
-                return {
-                  ...prevState,
-                  grafo: {
-                    ...prevState.grafo,
-                    end: e.target.value,
-                  },
-                };
-              });
             }}
           />
           <Grid container>
             <Grid item xs={3} sx={{ height: "100%" }}>
-              <Sidebar
-                addVertice={this.addVertice}
-                addEdge={this.addEdge}
-                vertices={this.state.grafo.vertices}
-              />
+              <Sidebar />
             </Grid>
             <Grid item xs={9} sx={{ height: "100%" }}>
-              <Editor
-                grafo={this.state.grafo}
-                modifyVertice={this.modifyVertice}
-                deleteVertice={this.deleteVertice}
-              />
+              <Editor />
             </Grid>
           </Grid>
         </div>
