ae566868ae1d495c05449a3574e580f2249fd7f6
ryzust <Zrubiol1700@alumno.ipn.mx>
2022-04-28 14:26:21 -0500

    added functionality to delete edges

--- src/Components/Connection/connection.component.jsx
+++ src/Components/Connection/connection.component.jsx
@@ -1,19 +1,27 @@
 import React from "react";
+import * as actions from "../../Actions";
+import { useDispatch } from "react-redux";
+import "./connection.styles.css";
 
-export const Connection = ({ p1, p2, gx }) => {
+export const Connection = ({ p1, p2, gx, v1, v2 }) => {
+  const dispatch = useDispatch();
   let left = Math.floor((p1[0] + p2[0]) / 2);
   let top = Math.floor((p1[1] + p2[1]) / 2);
 
+  const handleDelete = () => {
+    dispatch(actions.deleteEdge(v1, v2));
+  };
   return (
     <>
       <p
         style={{
-          zIndex: -1,
+          zIndex: 1,
           position: "absolute",
           top,
           left,
         }}
         className="connection"
+        onClick={handleDelete}
       >
         {gx}
       </p>
